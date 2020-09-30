a80154638af14d37034a38338bdadbb512fef845
Monica Wojciechowska <monica.wojciechowska@netguru.pl>
2020-10-01 00:55:54 +0200

    Updates

---
+++ precache-manifest.4fc9b622c27c1ecdc6cff774cee94825.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8f183e42ede4dbe726dd42c647a8c5",
+    "url": "/svg-yoga/index.html"
+  },
+  {
+    "revision": "72a1688bf95c5606b152",
+    "url": "/svg-yoga/static/css/main.c700336e.chunk.css"
+  },
+  {
+    "revision": "2b6741a40870310263a5",
+    "url": "/svg-yoga/static/js/2.33cc73cb.chunk.js"
+  },
+  {
+    "revision": "73dd3e3697a716729b64fe904848b202",
+    "url": "/svg-yoga/static/js/2.33cc73cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72a1688bf95c5606b152",
+    "url": "/svg-yoga/static/js/main.e81df3d9.chunk.js"
+  },
+  {
+    "revision": "9f0cd1cbe58ae5a3652b",
+    "url": "/svg-yoga/static/js/runtime-main.aa47335a.js"
+  }
+]);
