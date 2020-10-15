6c478c8a11b91ecd567a14424a7632dd5a3cb074
Monica Wojciechowska <monica.wojciechowska@netguru.pl>
2020-10-15 19:18:18 +0200

    feat: add google analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-68196341-4');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
